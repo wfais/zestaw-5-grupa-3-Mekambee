ab8e3d671c96906f22f0c68dad454b641725cef6
Mekambee <kamil.piechotaaax@gmail.com>
2024-12-16 18:59:58 +0100

    readme, zadanie2 skonczone

--- ZADANIE2/maximize_it.py
+++ ZADANIE2/maximize_it.py
@@ -4,9 +4,18 @@ from itertools import product
 
 def maximize_expression(K, M, lists):
     # twoj kod tutaj
-    pass 
+    combinations = product(*lists)
+    max_value = 0
 
+    for combination in combinations:
+        current_sum = sum(x**2 for x in combination)
 
+        current_mod_value = current_sum % M
+
+        if current_mod_value > max_value:
+            max_value = current_mod_value
+
+    return max_value
 
 if __name__ == "__main__":
     K, M = map(int, input().rstrip().split())
